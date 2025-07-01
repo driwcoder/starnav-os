353b1d7d75e1b682999aa23519a5220c85e8872c
driwCoder <driwcoder@gmail.com>
2025-07-01 17:53:25 -0300

    favicon e icon e header.tsx atualizados, novas imagens adicionadas

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,9 +3,17 @@
 
 import { signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react"; // Será necessário instalar lucide-react
 import Link from "next/link"; // Adicione esta importação
+import Image from "next/image";
 
 export default function Header() {
   const { data: session } = useSession();
@@ -24,27 +32,49 @@ export default function Header() {
             <SheetContent side="left">
               <SheetHeader>
                 <SheetTitle>Navegação</SheetTitle>
-                <SheetDescription>
-                  Menu principal do Starnav
-                </SheetDescription>
+                <SheetDescription>Menu principal do Starnav</SheetDescription>
               </SheetHeader>
               <nav className="flex flex-col space-y-4 pt-6">
-                <Link href="/" className="text-lg font-medium text-gray-700 hover:text-indigo-600">
+                <Link
+                  href="/"
+                  className="text-lg font-medium text-gray-700 hover:text-indigo-600"
+                >
                   Dashboard
                 </Link>
-                <Link href="/dashboard/service-orders" className="text-lg font-medium text-gray-700 hover:text-indigo-600">
+                <Link
+                  href="/dashboard/service-orders"
+                  className="text-lg font-medium text-gray-700 hover:text-indigo-600"
+                >
                   Ordens de Serviço
                 </Link>
               </nav>
             </SheetContent>
           </Sheet>
-          <h1 className="ml-4 text-2xl font-bold text-black">Starnav</h1>
+          <h1 className="ml-4 text-2xl font-bold text-black">
+            <Image
+              src="/STARNAV_ServicosMaritimosLtda_Branco_Preto.svg"
+              alt="Starnav"
+              width={250}
+              height={50}
+            />
+            {/* <Image
+            src="/STARNAV_Branco_Preto.png"
+            alt="Starnav"
+            width={50}
+            height={50}
+          />  */}
+          </h1>
         </div>
         <nav className="flex items-center space-x-4">
           {session?.user ? (
             <>
-              <span className="hidden text-gray-700 md:inline">Olá, {session.user.name || session.user.email}!</span>
-              <Button onClick={() => signOut({ callbackUrl: "/login" })} variant="outline">
+              <span className="hidden text-gray-700 md:inline">
+                Olá, {session.user.name || session.user.email}!
+              </span>
+              <Button
+                onClick={() => signOut({ callbackUrl: "/login" })}
+                variant="outline"
+              >
                 Sair
               </Button>
             </>
